685597aa4df8dddb49c63dc587fd49d742cd9ce6
Dilyar Naasan <dilyar.naasan@stud.thga.de>
2026-05-02 23:31:03 +0200

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+window.FIREBASE_SYNC_ENABLED = true;
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDECEf8ENv2kLe3VBj1AHAyAnZaeJJB6ZI",
+    authDomain: "habits-sync.firebaseapp.com",
+    projectId: "habits-sync",
+    appId: "1:511647865392:web:a4a360e2c93d1537a1abf5"
+};
